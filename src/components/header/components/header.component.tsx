c440ac91046d4a61794ec49ccc75b108dc21a377
AminAbdo <AminAbdo69>
2025-05-16 22:11:39 +0300

    feat: add Header component and integrate navigation bar into it

---
+++ src/components/header/components/header.component.tsx
@@ -0,0 +1,25 @@
+import logo from "../images/logo.svg";
+import { NavigationBar } from "./navigation-bar.component";
+
+export const Header = () => {
+  return (
+    <header className="max-w-[1440px] mx-auto relative">
+      <div className="absolute inset-0 px-[24px] lg:px-[170px] mx-auto mt-10">
+        <div className="flex justify-between items-center w-full">
+          <div className="flex-shrink-0">
+            <img
+              src={logo}
+              alt="Loop studios"
+              className="h-[24px] z-20"
+              width="144"
+              height="24"
+            />
+          </div>
+          <div className="flex-grow flex justify-end">
+            <NavigationBar />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
--- src/components/nav/index.ts
+++
@@ -1,1 +0,0 @@
-export { NavigationBar } from "./components/navigation-bar.component";
